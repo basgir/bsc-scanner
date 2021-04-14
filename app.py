ecd83e0db1551aef0eaa7dc3c881ac43d713d502
bastien.girardet <bastien.girardet@gmail.com>
2021-04-14 10:36:15 +0200

    Added default route

--- app.py
+++ app.py
@@ -50,5 +50,9 @@ def refresh_database():
     query_bsc()
     return retrieve_all_transaction().to_csv()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5786)
